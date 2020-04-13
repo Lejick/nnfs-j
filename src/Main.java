f6d4b8abfb76a09d12e0cebe90e0c30b63da3a0d
Lejick <lejick@gmail.com.com>
2020-04-13 13:56:47 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        float[] inputs = {1.2f, 5.1f, 2.1f};
+        float[] weights = {3.1f, 2.1f, 8.7f};
+        float bias = 3;
+        float output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias;
+        System.out.println(output);
+
+    }
+}
