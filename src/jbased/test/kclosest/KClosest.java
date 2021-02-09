056f30f2dc33c65417d4b663214d0865adf1d6d5
Oleg.Trohov <oleg.trohov@litota.ru>
2021-02-09 09:18:20 +0300

    .

--- src/jbased/test/kclosest/KClosest.java
+++ src/jbased/test/kclosest/KClosest.java
@@ -6,36 +6,59 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class KClosest {
-    private static Map<Point, Double> sharedMap = new HashMap<>();
+    private Map<Point, Double> sharedMap = new HashMap<>();
+    private Point[] sharedArray;
+    private int capacity;
 
-    public static List<Point> find(List<Point> inputList, Point x, int k) {
+    public KClosest(int k) {
+        sharedArray = new Point[k];
+        capacity = k;
+    }
+
+    public List<Point> findStreamStyle(List<Point> inputList, Point x) {
         Map<Point, Double> map = new HashMap<>();
         for (Point p : inputList) {
-            map.put(p, Math.sqrt(Math.pow(p.x - x.x, 2) + Math.pow(p.y - x.y, 2)));
+            map.put(p, x.distance(p));
         }
         Stream<Point> sorted =
                 map.entrySet().stream()
                         .sorted(Map.Entry.comparingByValue()).
-                        limit(k).map(pointDoubleEntry -> pointDoubleEntry.getKey());
+                        limit(capacity).map(pointDoubleEntry -> pointDoubleEntry.getKey());
         return sorted.collect(Collectors.toList());
     }
 
-    public static List<Point> find(Point newPoint, Point x, int k) {
-        sharedMap.put(newPoint, Math.sqrt(Math.pow(newPoint.x - x.x, 2) + Math.pow(newPoint.y - x.y, 2)));
+    public List<Point> findOnceStreamStyle(Point newPoint, Point x) {
+        sharedMap.put(newPoint, x.distance(newPoint));
         Stream<Point> sorted =
                 sharedMap.entrySet().stream()
                         .sorted(Map.Entry.comparingByValue()).
-                        limit(k).map(pointDoubleEntry -> pointDoubleEntry.getKey());
+                        limit(capacity).map(pointDoubleEntry -> pointDoubleEntry.getKey());
         return sorted.collect(Collectors.toList());
+    }
+
+    public Point[] findFixedArray(Point newPoint, Point x) {
+        for (int i = 0; i < sharedArray.length; i++) {
+            if (sharedArray[i] == null) {
+                sharedArray[i] = newPoint;
+                return sharedArray;
+            }
+        }
+        for (int i = 0; i < sharedArray.length; i++) {
+            if (newPoint.distance(x) < sharedArray[i].distance(x)) {
+                sharedArray[i] = newPoint;
+                return sharedArray;
+            }
+        }
+        return sharedArray;
     }
 
 }
 
 class Point {
-    int x;
-    int y;
+    Double x;
+    Double y;
 
-    public Point(int x, int y) {
+    public Point(Double x, Double y) {
         this.x = x;
         this.y = y;
     }
@@ -46,5 +69,9 @@ class Point {
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+
+    public Double distance(Point other) {
+        return Math.sqrt(Math.pow(other.x - x, 2) + Math.pow(other.y - y, 2));
     }
 }
--- src/jbased/test/kclosest/Test.java
+++ src/jbased/test/kclosest/Test.java
@@ -1,32 +1,37 @@
 package jbased.test.kclosest;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Stream;
 
 public class Test {
+    Point x = new Point(0D, 0D);
+    int k = 10;
+
     @org.junit.jupiter.api.Test
     public void test1() {
-        Point x = new Point(0, 0);
-        Point a = new Point(1, 1);
-        Point b = new Point(-1, 0);
-        Point c = new Point(2, 1);
+        Point x = new Point(0d, 0d);
+        Point a = new Point(1d, 1d);
+        Point b = new Point(-1d, 0d);
+        Point c = new Point(2d, 1d);
         List<Point> list = new ArrayList<>();
         list.add(a);
         list.add(b);
         list.add(c);
-        List<Point> resultList = KClosest.find(list, x, 2);
+        KClosest kClosest = new KClosest(2);
+        List<Point> resultList = kClosest.findStreamStyle(list, x);
         System.out.println(resultList);
     }
 
     @org.junit.jupiter.api.Test
     public void test2() {
-        Point x = new Point(0, 0);
+        KClosest kClosest = new KClosest(k);
         Random rand = new Random();
         while (true) {
-            Stream.generate(() -> new Point(rand.nextInt(10), rand.nextInt(10))).forEach(point -> {
-                List<Point> resultList = KClosest.find(point, x, 2);
+            Stream.generate(() -> new Point(rand.nextDouble() * 100, rand.nextDouble() * 100)).forEach(point -> {
+                List<Point> resultList = kClosest.findOnceStreamStyle(point, x);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -36,6 +41,23 @@ public class Test {
                 System.out.println(resultList);
             });
         }
+    }
 
+    @org.junit.jupiter.api.Test
+    public void test3() {
+
+        Random rand = new Random();
+        KClosest kClosest = new KClosest(k);
+        while (true) {
+            Stream.generate(() -> new Point(rand.nextDouble() * 100, rand.nextDouble() * 100)).forEach(point -> {
+                Point[] resultArray = kClosest.findFixedArray(point, x);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Arrays.toString(resultArray));
+            });
+        }
     }
 }
